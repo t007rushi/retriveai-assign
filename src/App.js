b7f87b2009b2ed10b5300fa01b005cf64f5ee188
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-06-11 00:37:05 +0530

    Drag Component created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import * as XLSX from "xlsx";
+import { DataTable } from "./Components/DataTable";
+import { Drag } from "./Components/Drag";
+import { Link, Route, Routes } from "react-router-dom";
+import { Vizualization } from "./Components/Vizualization";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [headers, setHeaders] = useState([]);
+  const [dropTableData, setdropTableData] = useState([]);
+
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+        const arrdata = XLSX.utils.sheet_to_json(ws, {
+          header: 1,
+          defVal: "",
+        });
+        setHeaders(arrdata[0]);
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      setItems(d);
+      console.log(d);
+      const final = d.reduce((acc, curr) => {
+        for (const k of Object.keys(curr)) {
+          if (!Object.keys(acc).includes(k)) {
+            acc = { ...acc, [k]: [curr[k]] };
+          } else {
+            acc = { ...acc, [k]: [...acc[k], curr[k]] };
+          }
+        }
+        return acc;
+      }, {});
+      setdropTableData(final);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <Link to="/">home</Link>
+        <Link to="/dnd">dnd</Link>
+        <Link to="/viz">viz</Link>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<DataTable items={items} headers={headers} />}
+        />
+        <Route
+          path="/dnd"
+          element={<Drag headers={headers} dropTableData={dropTableData} />}
+        />
+        <Route
+          path="/viz"
+          element={
+            <Vizualization headers={headers} dropTableData={dropTableData} />
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Drag.jsx
@@ -0,0 +1,112 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
+import { Draggable } from "react-beautiful-dnd";
+import { Droppable } from "react-beautiful-dnd";
+import { DragDropContext } from "react-beautiful-dnd";
+
+export const Drag = ({ headers, dropTableData }) => {
+  const [dropped, setDropped] = useState([]);
+  const [droppedTableToshow, setdroppedTableToshow] = useState({});
+  useEffect(() => {
+    var neobj = { ...droppedTableToshow };
+    for (const d of Object.keys(dropTableData)) {
+      if (dropped.includes(d)) {
+        neobj = { ...neobj, [d]: dropTableData[d] };
+      }
+    }
+    setdroppedTableToshow(neobj);
+
+  },[dropped])
+  const handleEnd = (result) => {
+    const { destination, source } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    let add;
+    let active = headers;
+    let complete = dropped;
+    // Source Logic
+    if (source.droppableId === "headerDrops") {
+      add = active[source.index];
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    // Destination Logic
+    if (destination.droppableId === "headerDrops2") {
+      complete.splice(destination.index, 0, add);
+    }
+    setDropped([...complete]);
+  };
+
+  return (
+    <DragDropContext onDragEnd={handleEnd}>
+      <div className="drag-drop">
+        <Droppable droppableId="headerDrops">
+          {(provided) => (
+            <div
+              className="draggable left"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              <h4>Available content to drag</h4>
+              {headers?.map((header, index) => {
+                return (
+                  <Draggable draggableId={header} index={index} key={index}>
+                    {(provided) => (
+                      <div
+                        className="dragItems"
+                        key={index}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                      >
+                        {header}
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        <Droppable droppableId="headerDrops2">
+          {(provided) => (
+            <div
+              className="draggable right"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {Object.keys(droppedTableToshow).map((head, index) => {
+                return (
+                  <tbody>
+                    <thead style={{ backgroundColor: "skyblue" }} key={index}>
+                      {head}
+                    </thead>
+                    {droppedTableToshow[head].map((val) => (
+                      <tr>
+                        <td>{val}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
+};
+
